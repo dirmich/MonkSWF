656394ffebb3ebde20681d214340bb48db26f8b4
Micah Pearlman <micahpearlman@gmail.com>
2010-05-22 14:10:01 -0700

    added gitignore and blank DoABC tags

---
+++ src/tags/DoABC.cpp
@@ -0,0 +1,16 @@
+/*
+ *  DoABC.cpp
+ *  MonkSWF
+ *
+ *  Created by Micah Pearlman on 5/22/10.
+ *  Copyright 2010 Zero Vision. All rights reserved.
+ *
+ */
+
+#include "DoABC.h"
+
+namespace MonkSWF {
+	bool DoABC::read( Reader* reader ) {
+		return true;
+	}
+}
